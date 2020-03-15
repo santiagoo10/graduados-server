3117e163ce659c59c242d457dcba0e9fe59e55c7
rene <renerecalde2@gmail.com>
2020-03-15 17:58:35 -0300

    Add security config.

---
+++ src/Security/Role.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Security;
+
+abstract class Role{
+
+   public const ROLE_USER ='ROLE_USER';
+   public const ROLE_ADMIN ='ROLE_ADMIN';
+   public const ROLE_SUPER_ADMIN = 'ROLE_SUPER_ADMIN';
+   public const ROLE_ALLOWED_TO_SWITCH= 'ROLE_ALLOWED_TO_SWITCH';
+
+}
+
