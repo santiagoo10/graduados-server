27b482d2a8ac13a0b1bbb8f1b7ff5f9acf670b91
rene <renerecalde2@gmail.com>
2020-03-22 19:03:54 -0300

    Fix entity.

--- src/Entity/SaleType.php
+++ src/Entity/SaleType.php
@@ -10,7 +10,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource()
- * @ORM\entity(repositoryClass="App\Repository\SaleTypeRepository")
+ * @ORM\Entity(repositoryClass="App\Repository\SaleTypeRepository")
  * @ORM\HasLifecycleCallbacks()
  */
 class SaleType
