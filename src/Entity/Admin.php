cd5cd9ab0782282b34ca1f799e1493793134077a
renerecalde <renerecalde2@gmail.com>
2020-05-20 12:40:30 -0300

    Add admin fixture.

--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -11,11 +11,16 @@ use App\Security\Role;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  *
  * @ApiResource(
- *     collectionOperations={"get", "post"},
+ *     collectionOperations={
+ *      "get" = {
+ *          },
+ *      "post"
+ *      },
  *     itemOperations={"get", "put", "delete"={"method"="DELETE"}},
  *     attributes={ "pagination_per_page"= 10},
  *     normalizationContext={"groups"={"admin:read"}},
@@ -60,6 +65,19 @@ class Admin extends User
      * @ORM\Column(type="string", length=255)
      * @ApiProperty(iri="http://schema.org/name")
      * @Groups({"admin:read", "admin:write"})
+     * @Assert\Length(
+     *      min = 10,
+     *      max = 11,
+     *      minMessage = "Your cuit must be at least 7 characters long",
+     *      maxMessage = "Your cuit cannot be longer than 9 characters",
+     *      allowEmptyString = false
+     * )
+     * @Assert\Regex(
+     *     pattern="/\s/",
+     *     match=false,
+     *     message="Your cuit cannot contain a string"
+     * )
+     * @Assert\Type(type={"numeric"})
      */
     private $cuit;
 
