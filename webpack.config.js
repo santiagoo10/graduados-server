4fbf39b9f57dac869b3150b70a60e325a74c8fda
Recalde Rene <rrecalde@santafe.gov.ar>
2020-09-23 09:27:37 -0300

    Limpia archivos frontend.

--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,6 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     // .addEntry('app', './assets/js/app.js')
-    .addEntry('app', './assets/js/app.js')
-    .addEntry('admin', './assets/js/admin.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -70,7 +68,7 @@ Encore
     //.autoProvidejQuery()
 
     // uncomment if you use API Platform Admin (composer req api-admin)
-    .enableReactPreset()
+    // .enableReactPreset()
 ;
 
 module.exports = Encore.getWebpackConfig();
